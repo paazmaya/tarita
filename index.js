37128646ba3f07794068ee50462f3d41e5938c16
Juga Paazmaya <paazmaya@yahoo.com>
2016-01-22 10:48:10 +0200

    Methods in places

--- index.js
+++ index.js
@@ -12,11 +12,149 @@
 const espree = require('espree'),
   escodegen = require('escodegen');
 
+
+/**
+ * Read the values used in define array and map to the function arguments
+ *
+ * @param {object} expression Top level expression object
+ * @returns {object} Map to be used for defining import statements
+ */
+const defineToImports = (expression) => {
+  const imports = {};
+
+  if (expression.type === 'CallExpression') {
+    console.log('callee.name: ' + expression.callee.name); // Should be define or require
+
+    if (expression.arguments[0].type === 'ArrayExpression') {
+      const identifiers = expression.arguments[0].elements.map((item) => {
+        return item.value;
+      });
+
+      // the first item should be array of file identifiers
+      // the second a function
+
+      const locals = expression.arguments[1].params.map((item) => item.name);
+
+      // FunctionExpression params are then mapped to the identifiers
+      locals.forEach((item, index) => {
+        imports[item] = identifiers[index];
+      });
+
+    }
+  }
+  return imports;
+};
+
+/**
+ * Search for the return statement from the end of the main function body
+ *
+ * @param {array} body Contents of the main function body
+ * @returns {array} Name of the function that should be exported and rest of the body
+ */
+const getNameAndContents = (body) => {
+  // Starting from end, look for ReturnStatement
+
+  let returnStatement = false;
+  body.reverse();
+  const nameIndex = body.findIndex((item) => {
+    return item.type && item.type === 'ReturnStatement';
+  });
+  if (typeof nameIndex === 'number') {
+    returnStatement = body[nameIndex].argument.name;
+    body.splice(nameIndex, 1);
+  }
+  body.reverse();
+
+  return {
+    defaultExportName: returnStatement,
+    contents: body
+  };
+};
+
+/**
+ * Create AST for import rules
+ *
+ * @param {object} imports Map for import statements
+ * @returns {array} AST for import declarations
+ */
+const addAstImports = (imports) => {
+  return Object.keys(imports).map((key) => {
+    const name = imports[key];
+    return {
+      type: 'ImportDeclaration',
+      specifiers: [
+        {
+          type: 'ImportDefaultSpecifier',
+          local: {
+            type: 'Identifier',
+            name: key
+          }
+        }
+      ],
+      source: {
+        type: 'Literal',
+        value: name
+      }
+    };
+  });
+};
+
+/**
+ * Create AST for default export declaration
+ *
+ * @param {string} name Name of the exported item
+ * @returns {array} AST for default export declaration
+ */
+const addAstExport = (name) => {
+  return [{
+    type: 'ExportDefaultDeclaration',
+    declaration: {
+      type: 'Literal',
+      value: name
+    }
+  }];
+};
+
+/**
+ * Process the AST in the hope of finding expression at the top
+ *
+ * @param {object} ast AST
+ * @returns {object} Possibly modified AST
+ */
+const process = (ast) => {
+
+  // body is array, which is require.js format should contain only one item
+
+  if (ast.body[0].type === 'ExpressionStatement') {
+    const impr = defineToImports(ast.body[0].expression);
+
+    if (ast.body[0].expression.arguments[1].body.type === 'BlockStatement') {
+
+      // Main function body. Usually ends with ReturnStatement
+
+      const divided = getNameAndContents(ast.body[0].expression.arguments[1].body.body);
+
+      const importList = addAstImports(impr);
+
+      const exportName = addAstExport(divided.defaultExportName);
+
+      const outputAst = importList.concat(divided.contents, exportName);
+      ast.body = outputAst;
+
+      ast.sourceType = 'module';
+    }
+
+  }
+  return ast;
+};
+
 module.exports = (input) => {
-  const ast = espree.parse(input, {
-    ecmaVersion: 6, sourceType: 'module'
+  let ast = espree.parse(input, {
+    ecmaVersion: 6,
+    sourceType: 'module'
   });
 
+  ast = process(ast);
 
   const output = escodegen.generate(ast);
   return output;
