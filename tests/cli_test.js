2937f936e039dc4a49dab9455feb029b25e1ff11
Juga Paazmaya <paazmaya@yahoo.com>
2016-08-08 17:36:06 +0300

    Unifying with other similar command line projects to get :neckbeard:

--- tests/cli_test.js
+++ tests/cli_test.js
@@ -16,13 +16,12 @@ const fs = require('fs'),
 
 const tape = require('tape');
 
-const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8')),
-  cli = 'bin/tarita.js';
+const pkg = JSON.parse(fs.readFileSync(path.join(__dirname, '..', 'package.json'), 'utf8'));
 
 tape('cli should output version number', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '-V'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '-V'], null, (err, stdout) => {
     test.equals(stdout.trim(), pkg.version, 'Version is the same as in package.json');
   });
 
@@ -31,7 +30,7 @@ tape('cli should output version number', (test) => {
 tape('cli should output help by default', (test) => {
   test.plan(1);
 
-  execFile('node', [cli], null, (err, stdout) => {
+  execFile('node', [pkg.bin], null, (err, stdout) => {
     test.ok(stdout.trim().indexOf('tarita [options] <file|directory>') !== -1, 'Help appeared');
   });
 
@@ -40,7 +39,7 @@ tape('cli should output help by default', (test) => {
 tape('cli should output help when requested', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '--help'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '--help'], null, (err, stdout) => {
     test.ok(stdout.trim().indexOf('tarita [options] <file|directory>') !== -1, 'Help appeared');
   });
 
@@ -49,7 +48,7 @@ tape('cli should output help when requested', (test) => {
 tape('cli should create folder for output', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '-o', 'tmp', 'tests/fixtures'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '-o', 'tmp', 'tests/fixtures'], null, (err, stdout) => {
     test.ok(fs.existsSync('tmp'), 'Temporary folder exists');
   });
 
@@ -58,7 +57,7 @@ tape('cli should create folder for output', (test) => {
 tape('cli should use verbose messages', (test) => {
   test.plan(2);
 
-  execFile('node', [cli, '-v', 'not-here'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '-v', 'not-here'], null, (err, stdout) => {
     test.ok(stdout.trim().indexOf('Going to process total of') !== -1, 'Verbose text seen for total');
     test.ok(stdout.trim().indexOf('Outputting to directory:') !== -1, 'Verbose text seen for output directory');
   });
@@ -68,7 +67,7 @@ tape('cli should use verbose messages', (test) => {
 tape('cli should show current file when verbose', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '-v', '-o', 'tmp', 'tests/fixtures/single-item.js'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '-v', '-o', 'tmp', 'tests/fixtures/single-item.js'], null, (err, stdout) => {
     test.ok(stdout.trim().indexOf('Processing file ') !== -1, 'Verbose text seen');
   });
 
@@ -82,7 +81,7 @@ tape('cli should complain when package.json is gone', (test) => {
 
   fs.renameSync(nameFrom, nameTo);
 
-  execFile('node', [cli, '-h'], null, (err, stdout, stderr) => {
+  execFile('node', [pkg.bin, '-h'], null, (err, stdout, stderr) => {
     test.ok(stderr.trim().indexOf('Could not read') !== -1, 'Complaint seen');
     fs.renameSync(nameTo, nameFrom);
   });
@@ -92,7 +91,7 @@ tape('cli should complain when package.json is gone', (test) => {
 tape('cli should complain when non existing option used', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '-g'], null, (err, stdout, stderr) => {
+  execFile('node', [pkg.bin, '-g'], null, (err, stdout, stderr) => {
     test.ok(stderr.trim().indexOf('Invalid option ') !== -1, 'Complaint seen');
   });
 
@@ -101,7 +100,7 @@ tape('cli should complain when non existing option used', (test) => {
 tape('cli should use matches when given', (test) => {
   test.plan(1);
 
-  execFile('node', [cli, '-v', '-o', 'tmp', '-M', '\\.jsx$', 'tests/fixtures'], null, (err, stdout) => {
+  execFile('node', [pkg.bin, '-v', '-o', 'tmp', '-M', '\\.jsx$', 'tests/fixtures'], null, (err, stdout) => {
     test.ok(stdout.trim().indexOf('/react-component.jsx') !== -1, 'React component file seen via verbosity');
   });
 
